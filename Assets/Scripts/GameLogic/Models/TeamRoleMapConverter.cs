470829491ccb24065d0b18df8bdaa2be0eb93754
AVAVT <itsavavt@gmail.com>
2018-12-29 23:23:56 +0700

    zzz

--- Assets/Scripts/GameLogic/Models/TeamRoleMapConverter.cs
+++ Assets/Scripts/GameLogic/Models/TeamRoleMapConverter.cs
@@ -7,14 +7,12 @@ public class TeamRoleMapConverter<T> : JsonConverter
 {
   public override bool CanConvert(Type objectType)
   {
-    UnityEngine.Debug.Log("AAA");
     if (objectType == typeof(TeamRoleMap<>)) return true;
     else return false;
   }
 
   public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
   {
-    UnityEngine.Debug.Log("CCC");
     JObject jObject = JObject.Load(reader);
     var map = jObject.ToObject<Dictionary<Team, Dictionary<Role, T>>>();
     return new TeamRoleMap<T>(map);
@@ -22,7 +20,6 @@ public class TeamRoleMapConverter<T> : JsonConverter
 
   public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
   {
-    UnityEngine.Debug.Log("BBB");
     writer.WriteRawValue((value as TeamRoleMap<T>).ToJSON());
     writer.WriteEndObject();
   }
