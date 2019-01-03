68f4e90827c6dd578c27a118c93ff8ce231f28c1
AVAVT <itsavavt@gmail.com>
2019-01-03 08:40:55 +0700

    js server template

--- ServerPackages/Javascript/index.js
+++ ServerPackages/Javascript/index.js
@@ -3,6 +3,14 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+const validActions = [
+  "UP",
+  "DOWN",
+  "LEFT",
+  "RIGHT",
+  "STAY"
+];
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json({ extended: false }));
 
@@ -18,25 +26,9 @@ app.post('/turn', (req, res) => {
   res.send('DOWN');
 });
 
-app.post('/timeout', (req, res) => {
-  const data = JSON.parse(req.body.data);
-  console.log(`Timeout test for team ${data.team} - role ${data.role}`);
-});
-
-app.post('/crash', (req, res) => {
-  const data = JSON.parse(req.body.data);
-  console.log(`Crash test for team ${data.team} - role ${data.role}`);
-  res.sendStatus(500);
-});
-
 app.post('/name', (req, res) => {
   console.log(`Name test`);
   res.send("Test Player Name");
-});
-
-app.post('/namecrash', (req, res) => {
-  console.log(`Name crash test`);
-  res.sendStatus(500);
 });
 
 const port = process.env.port || 8484;
